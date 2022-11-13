d05cf1afb02ff1d47187ee9ff5ea4c35ad083578
Belén López <belop_18@hotmail.com>
2022-11-12 23:23:39 -0300

    Filtro y ordenamiento

--- src/app/components/listado-mascota/listado-mascota.component.ts
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Mascota } from 'src/app/interfaces/mascota';
 
@@ -23,6 +24,7 @@ export class ListadoMascotaComponent implements OnInit, AfterViewInit  {
   dataSource = new MatTableDataSource<Mascota>(listMascotas);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor() { }
 
@@ -31,7 +33,13 @@ export class ListadoMascotaComponent implements OnInit, AfterViewInit  {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     this.paginator._intl.itemsPerPageLabel = 'Items por página'
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
